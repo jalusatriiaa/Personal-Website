ebe3d1be668b39a1c129ef97d39285c1b98d3398
Jalu Satria Pratama <jalusatria17@gmail.com>
2020-03-08 22:43:15 +0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function closeMenu(){
+    document.getElementById("sidenavbar").style.height = "0%";
+  }
+  function openMenu(){
+    document.getElementById("sidenavbar").style.height = "100%";
+  }
