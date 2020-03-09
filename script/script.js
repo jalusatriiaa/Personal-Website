a3b1d5ea37314fe32bff00d8ef7e47f70dea210c
Jalu Satria Pratama <jalusatria17@gmail.com>
2020-03-09 12:28:29 +0700

    Add Animation & Fix position

--- script/script.js
+++ script/script.js
@@ -1,6 +1,45 @@
 function closeMenu(){
     document.getElementById("sidenavbar").style.height = "0%";
   }
-  function openMenu(){
+function openMenu(){
     document.getElementById("sidenavbar").style.height = "100%";
   }
+
+// Scrolled Animated
+
+$(window).scroll(function(){
+  let wScroll = $(this).scrollTop();
+
+  if(wScroll > 150){
+    $('.gambar img').addClass('animated fadeInLeft slow')
+    $('.aboutme').addClass('animated fadeInRight slow')
+  }
+
+  if(wScroll > 500){
+    $('.portofolio').addClass('animated slideInDown slow')
+    $('.port1').addClass('animated slideInDown slow')
+    $('.port2').addClass('animated slideInDown slow')
+    $('.port3').addClass('animated slideInDown slow')
+    $('.port4').addClass('animated slideInDown slow')
+  }
+
+  if(wScroll>20){
+
+  }
+
+
+});
+
+// Sticky Navbar
+
+let myNavigation = $('navbar');
+    stickyNavbar = "sticky-navbar";
+    myHeader = $('.main').height();
+
+    $(window).scroll(function(){
+      if($(this).scrollTop() > myHeader){
+        myNavigation.addClass(stickyNavbar);
+      }else{
+        myNavigation.removeClass(stickyNavbar);
+      }
+    });
